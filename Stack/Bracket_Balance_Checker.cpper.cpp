73d5a7a2d0476382c9565c9db775a71f1c238299
Vivaan Gupta <155184535+vivaan3141@users.noreply.github.com>
2026-01-05 22:40:39 +0530

    Create Bracket_Balance_Checker.cpper.cpp

---
+++ Stack/Bracket_Balance_Checker.cpper.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+int main() {
+    string s; cin >> s; stack<char> a;
+
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '(' ) {
+            a.push(s[i]);
+        }else {
+            if (!a.empty()) {
+                a.pop();
+            }
+        }
+    }
+    
+    if (a.empty()) {
+        cout << "Balanced" << endl;
+    } else {
+        cout << "Not Balcanced" << endl;
+    }
+}
